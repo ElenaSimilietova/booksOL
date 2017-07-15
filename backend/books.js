8e3ff2061ab8e9e6ab112395147385a670c40759
Lasse Kallio <lasse11@dlc.fi>
2017-07-15 12:28:49 +0300

    sql selects bookMostPop, booksPagesNum.. added

--- backend/books.js
+++ backend/books.js
@@ -5,7 +5,7 @@ exports.getBookById = function(req, res){
   var db = sql.database_connect();
   var id = req.params.id;
 
-  db.query('SELECT id, name, id_genre, big_pic, description FROM books WHERE id = ?',[id], function (err, rows) {
+  db.query('SELECT a.name, g.name, b.id, b.name, b.id_genre, b.big_pic, b.description FROM books b, authors a, genres g WHERE b.id_author = a.id and b.id_genre = g.id and b.id = ?',[id], function (err, rows) {
     var result;
     if (err) {
       result = {'data': 'SQL error'};
@@ -17,4 +17,41 @@ exports.getBookById = function(req, res){
     return result;
  }) 
   
+};
+
+exports.booksMostPop = function(req, res){
+
+  var db = database_connect();
+  var num = req.params.num;  
+
+  db.query("SELECT id, name, id_author, small_pic, sum_points/votes_number as 'rating' FROM books order by rating desc limit " + [num], function (err, rows) {
+    var result;
+    if (err) {
+      result = {'data': 'SQL error'};
+    }
+    else {
+      result = res.json(rows[0]);
+    }
+    db.end();
+    return result;
+ }) 
+
+};
+exports.booksPagesNumber = function(req, res){
+
+  var db = database_connect();
+  var num = req.params.id;  
+
+  db.query('SELECT id, name, pages_number FROM books where id = ?',[id], function (err, rows) {
+    var result;
+    if (err) {
+      result = {'data': 'SQL error'};
+    }
+    else {
+      result = res.json(rows[0]);
+    }
+    db.end();
+    return result;
+ }) 
+
 };
