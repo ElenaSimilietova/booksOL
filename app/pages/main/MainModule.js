9f3405aaec94d85e7c0f0fb3029bb5f888611655
Olena <similyetova@gmail.com>
2017-10-02 20:22:13 +0300

    Main page fixes

--- app/pages/main/MainModule.js
+++ app/pages/main/MainModule.js
@@ -11,8 +11,25 @@ angular.module('mainModule', ['ngRoute', 'bookFactoryModule', 'popularAuthorsMod
 
 .controller('MainController', ['$scope', 'Book', function($scope, Book) {
   $scope.message = null;
+  
   Book.getMostPopular(6).then(function(response) {
-    $scope.books = response.data;
+    $scope.books = [];
+    var row = 0,
+        col = 0;
+    response.data.forEach(function(book) {
+
+      if (col == 0) {
+        $scope.books[row] = [];
+        $scope.books[row].push(book);
+      } else {
+        $scope.books[row].push(book);
+      }
+      col++;
+      if (col == 2) {
+        col = 0;
+        row++;
+      }
+    });
   }, function(reason) {
     $scope.message = 'Sorry, but something went wrong.';
   });
